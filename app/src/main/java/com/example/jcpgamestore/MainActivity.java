78c0a1e84b34349bfa464ed6231d96492ad53b63
Paulo <rech.paulog@gmail.com>
2019-10-19 19:23:17 -0700

    Add search into the Actionbar

--- app/src/main/java/com/example/jcpgamestore/MainActivity.java
+++ app/src/main/java/com/example/jcpgamestore/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.jcpgamestore;
 import androidx.appcompat.app.AppCompatActivity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -51,14 +54,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Basic Action Bar Set for the Search Bar
-        //Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        //setSupportActionBar(myToolbar);
-
         addItems();
-        final ListView listViewGames = findViewById(R.id.listGames );
+        ListView listViewGames = findViewById(R.id.listGames );
         listViewGames.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, gameList));
-        final MyCustomAdapter myAdapter = new MyCustomAdapter( this,gameList, gamePic );
+        MyCustomAdapter myAdapter = new MyCustomAdapter( this,gameList, gamePic );
         listViewGames.setAdapter( myAdapter );
         listViewGames.setOnItemClickListener( new AdapterView.OnItemClickListener() {
             @Override
@@ -88,5 +87,26 @@ public class MainActivity extends AppCompatActivity {
         } );
 
     }
+    //Search widget in Menu Bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate( R.menu.menu_search, menu );
+        MenuItem item = menu.findItem( R.id.menuSearch );
+        SearchView searchView = (SearchView)item.getActionView();
 
+        searchView.setOnQueryTextListener( new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        } );
+
+        return super.onCreateOptionsMenu( menu );
+    }
 }
