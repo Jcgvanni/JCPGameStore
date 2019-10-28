4543f610f5cfea4a6ae6dee8c77bb2fbdf5f7c09
Paulo <rech.paulog@gmail.com>
2019-10-28 12:00:24 -0700

    Add Default LoginActivity with added logo. Added Splash Activity. Added Logo to Icon. Removed Action Bar from Splash Activity and Login Activity.

---
+++ app/src/main/java/com/example/jcpgamestore/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.jcpgamestore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_splash );
+
+        //define timertask
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        };
+
+        //define a timer
+        Timer timer = new Timer(  );
+        timer.schedule( timerTask, 3000 );
+    }
+}
